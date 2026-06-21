6893bf3b25bbd0cff63c91b27f9f78e7168560d5
Anshu Singh <anshuusingh67@gmail.com>
2026-06-21 21:41:33 +0530

    connect render backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://mobidrag-admin-portal.onrender.com/api",
 });
 
 // Attach the stored token to every outgoing request
